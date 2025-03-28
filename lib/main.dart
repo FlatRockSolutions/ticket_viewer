c596ce65c1b165be488bf8c5b5533feee876cb17
Lekau Mamabolo <lekau9222@gmail.com>
2025-03-29 00:53:20 +0200

    add and implement Bloc for ticket state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ticket_viewer/routes/app_router.dart';
+import 'package:ticket_viewer/blocs/ticket_bloc.dart';
+import 'package:ticket_viewer/data/ticket_api.dart';
+import 'package:ticket_viewer/data/asset_interceptor.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      title: 'Ticket Viewer',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final dio = Dio();
+    dio.interceptors.add(AssetInterceptor());
+
+    return BlocProvider(
+      create: (context) => TicketBloc(TicketApi(dio)),
+      child: MaterialApp.router(
+        routerConfig: router,
+        title: 'Ticket Viewer',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
