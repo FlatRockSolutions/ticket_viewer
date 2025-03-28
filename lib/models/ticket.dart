b3448682c7cb9de55fc16b1c2d82b967cfb47550
Lekau Mamabolo <lekau9222@gmail.com>
2025-03-29 00:51:55 +0200

    added ticket model

---
+++ lib/models/ticket.dart
@@ -0,0 +1,23 @@
+class Ticket {
+  final int id;
+  final String title;
+  final String description;
+  final String status;
+  final String createdAt;
+
+  Ticket(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.status,
+      required this.createdAt});
+
+  factory Ticket.fromJson(Map<String, dynamic> json) {
+    return Ticket(
+        id: json['id'],
+        title: json['title'],
+        description: json['description'],
+        status: json['status'],
+        createdAt: json['created_at']);
+  }
+}
