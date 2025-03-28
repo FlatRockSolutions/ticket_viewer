8a8a7abb3fa301f06420c8ff57921292c2cef077
Lekau Mamabolo <lekau9222@gmail.com>
2025-03-29 00:01:55 +0200

    Refactor routing: Move GoRouter setup to a new app_router.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:ticket_viewer/screens/ticket_detail_screen.dart';
-import 'package:ticket_viewer/screens/ticket_list_screen.dart';
+import 'package:ticket_viewer/routes/app_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-final router = GoRouter(
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const TicketListScreen()),
-    GoRoute(
-        path: '/ticket/:id',
-        builder: (context, state) =>
-            TicketDetailScreen(id: state.pathParameters['id'] ?? '')),
-  ],
-);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
